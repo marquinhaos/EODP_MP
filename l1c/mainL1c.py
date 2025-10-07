aee58fc8c947ce2ac1299735f83751e699a6b104
marquinhaos <portomarcosv@gmail.com>
2025-10-07 22:32:17 +0200

    lab 1

--- l1c/mainL1c.py
+++ l1c/mainL1c.py
@@ -4,10 +4,10 @@
 from l1c.src.l1c import l1c
 
 # Directory - this is the common directory for the execution of the E2E, all modules
-auxdir = '/home/luss/EODP/eodp/auxiliary'
+auxdir = "C:/Users/Marcos Porto/Desktop/EODP/EODP_MP/auxiliary"
 # GM dir + L1B dir
-indir = '/home/luss/my_shared_folder/gm_out/gm_alt100_act_150/,/home/luss/EODP/eodp/l1b/test/ut02/output'
-outdir = '/home/luss/EODP/eodp/l1c/test/ut01/output'
+indir = 'C:/Users/Marcos Porto/Desktop/EODP/EODP-TS-L1C/input'
+outdir = 'C:/Users/Marcos Porto/Desktop/EODP/EODP-TS-L1C/myout'
 
 # Initialise the ISM
 myL1c = l1c(auxdir, indir, outdir)
